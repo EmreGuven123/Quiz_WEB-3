e8e783d758c309c4253ac7399e432922b3d07422
EmreGuven123 <97890309+EmreGuven123@users.noreply.github.com>
2023-10-02 22:23:25 +0300

    Add files via upload

---
+++ BilgiYarimasiWEB/Login.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using BilgiYarimasiWEB.Classes;
+using System.Data;
+
+namespace BilgiYarimasiWEB
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnlogin_Click(object sender, EventArgs e)
+        {
+            SqlCommand commandLogin = new SqlCommand("SELECT * FROM SkorKayit WHERE Name=@pname AND Surname=@pSname", SqlConnecitonClass.connection);
+            SqlConnecitonClass.CheckConnection();
+            commandLogin.Parameters.AddWithValue("@pname", tboxName.Text);
+            commandLogin.Parameters.AddWithValue("@pSname", tboxSurname.Text);
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(commandLogin);
+
+            da.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                Response.Write("Giriş yapıldı");
+                Response.Redirect("Sorular.aspx");
+            }
+            else
+            {
+                Response.Write("Kullanıcı adı veya Soyadı Hatalı");
+            }
+        }
+    }
+}
---
+++ BilgiYarimasiWEB/WebForm1.aspx.cs
@@ -0,0 +1,37 @@
+﻿using BilgiYarimasiWEB.Classes;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BilgiYarimasiWEB
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Session["DogruSayisi"] != null && Session["YanlisSayisi"] != null)
+                {
+                    int dogruSayisi = (int)Session["DogruSayisi"];
+                    int yanlisSayisi = (int)Session["YanlisSayisi"];
+
+                    LBLTrueCount.Text = dogruSayisi.ToString();
+                    LBLFalseCount.Text = yanlisSayisi.ToString();
+                }
+            }
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            Response.Redirect("Sorular.aspx");
+        }
+
+    }
+}
